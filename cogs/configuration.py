a29717f0e5040bf093fb78e453258b972b1786c4
Jack Arthur Harrhy <me@jackharrhy.com>
2021-01-04 11:20:10 -0330

    python black *.py cogs/*.py

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -8,8 +8,10 @@ count_db = dataset.connect(COUNT_DB)["table"]
 DEFAULT_PREFIX = "'"
 general_error = "Something went wrong! Feel free to submit an issue at https://github.com/theteamaker/chii."
 
+
 def setup(bot):
     bot.add_cog(Configuration(bot))
+
 
 def get_prefix(bot, message):
     server = servers_db.find_one(server_id=message.guild.id)
@@ -20,24 +22,27 @@ def get_prefix(bot, message):
                 return server["prefix"]
         except:
             pass
-    
+
     return DEFAULT_PREFIX
+
 
 class Configuration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     async def has_permission(ctx):
         check = ctx.message.author.guild_permissions.manage_channels
         bot_owner_id = int(BOT_OWNER_ID)
 
         if bot_owner_id == ctx.message.author.id:
             return True
-        
+
         if check is True:
             return check
-        
-        await ctx.send("You need the `Manage Channels` permission to change the prefix of this bot.")
+
+        await ctx.send(
+            "You need the `Manage Channels` permission to change the prefix of this bot."
+        )
         return check
 
     @commands.command()
@@ -49,31 +54,40 @@ class Configuration(commands.Cog):
         if len(args) != 1:
             await ctx.send(usage)
             return
-        
+
         if re.match(r"^.{,5}$", args[0]) is None:
-            await ctx.send("**Error:** The specified prefix for this bot must be *at most* 5 characters, and contain no spaces.")
+            await ctx.send(
+                "**Error:** The specified prefix for this bot must be *at most* 5 characters, and contain no spaces."
+            )
             return
-        
+
         try:
-            servers_db.upsert(dict(server_id=int(ctx.guild.id), prefix=args[0]), ["server_id"])
-            await ctx.send(f"The bot's prefix for this server has been successfully set to `{args[0]}`!")
+            servers_db.upsert(
+                dict(server_id=int(ctx.guild.id), prefix=args[0]), ["server_id"]
+            )
+            await ctx.send(
+                f"The bot's prefix for this server has been successfully set to `{args[0]}`!"
+            )
         except:
             await ctx.send(general_error)
+
 
 async def limit_safe(ctx):
     count = count_db.find_one(name="count")
 
     if count is not None:
-            if count["total"] >= 3500000:
-                await ctx.send("The maximum character limit has been reached! Please consult the bot creator.")
-                return False
-            else:
-                return True
-    
+        if count["total"] >= 3500000:
+            await ctx.send(
+                "The maximum character limit has been reached! Please consult the bot creator."
+            )
+            return False
+        else:
+            return True
+
     if count is None:
         return True
+
 
 async def bot_author(ctx):
     bot_owner_id = int(BOT_OWNER_ID)
     return ctx.message.author.id == bot_owner_id
-    
--- cogs/nightcoreify.py
+++ cogs/nightcoreify.py
@@ -2,29 +2,29 @@ from discord.ext import commands
 import youtube_dl, tempfile, discord
 from env import TEMP_DIR
 
+
 def nightcoreify(rate, link):
     ydl_opts = {
-        'format': 'bestaudio',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'opus',
-            'preferredquality': '128',
-        }]
+        "format": "bestaudio",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "opus",
+                "preferredquality": "128",
+            }
+        ],
     }
 
     with tempfile.NamedTemporaryFile(dir=TEMP_DIR) as t:
         filename = str(t.name) + "_ytdl.opus"
-        ydl_opts = {
-        'format': 'bestaudio',
-        'outtmpl': filename
-        }
+        ydl_opts = {"format": "bestaudio", "outtmpl": filename}
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
-        
+
         source = discord.FFmpegOpusAudio(
-            filename, 
-            options=f'-filter_complex [0:a:0]volume=0.15,asetrate={rate}*48k,aresample=resampler=soxr:precision=24:osf=s32:tsf=s32p:osr=48k[out] -map [out]'
-            )
-    
+            filename,
+            options=f"-filter_complex [0:a:0]volume=0.15,asetrate={rate}*48k,aresample=resampler=soxr:precision=24:osf=s32:tsf=s32p:osr=48k[out] -map [out]",
+        )
+
         return source, filename
